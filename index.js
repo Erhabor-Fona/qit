0f787fa796cba6ee2ed77b2037470d7fb37aa170
prime-infinity <soe.a.prime@gmail.com>
2024-10-23 06:37:51 -0700

    testing with branch

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
 console.log("Program started");
+const fs = require("fs");
+const path = require("path");
+
+function getConfig() {
+  try {
+    // Try to read config file from current working directory
+    const configPath = path.join(process.cwd(), "qit.config.json");
+    const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+    return {
+      branch: config.branch || "main", // Use 'main' if branch is not specified in config
+    };
+  } catch (error) {
+    // Return default config if file doesn't exist or has errors
+    return {
+      branch: "main",
+    };
+  }
+}
+
+const config = getConfig();
+console.log(`Current branch: ${config.branch}`);
